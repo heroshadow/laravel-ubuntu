1a2119c2b957523525389af9f85599f054d89c89
wen4434 <wen4434@126.com>
2016-08-28 18:18:33 +0800

    laravel

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,7 +9,7 @@ class ExampleTest extends TestCase
     /**
      * A basic functional test example.
      *
-     * @return void
+     * @return voidld
      */
     public function testBasicExample()
     {
